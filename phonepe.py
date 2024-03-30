c922df21889aeb8a3f7c3479d0c1277b10a2ec08
Devanathan Raja <154589184+devadhanush2104@users.noreply.github.com>
2024-03-30 12:17:49 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -853,7 +853,8 @@ elif select == "DATA EXPLORATION":
 
             col1,col2= st.columns(2)
             with col1:
-                states= st.selectbox("Select The State", map_insur_tac_Y["States"].unique())
+                states = st.selectbox("Select The State", map_insur_tac_Y["States"].unique(), key="unique_key_for_state_selectbox")
+
 
             Map_insur_District(map_insur_tac_Y, states)
 
